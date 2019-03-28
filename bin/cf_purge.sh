e547fcf2803cf7bc27c2d5ff909ef2bcd33b3798
Chris Short <chris@chrisshort.net>
2019-03-28 14:13:53 -0400

    Just work please
    Signed-off-by: Chris Short <chris@chrisshort.net>

--- bin/cf_purge.sh
+++ bin/cf_purge.sh
@@ -4,4 +4,5 @@ curl -X POST "https://api.cloudflare.com/client/v4/zones/${CF_ZONE_ID}/purge_cac
      -H "X-Auth-Email: ${CF_EMAIL}" \
      -H "X-Auth-Key: ${CF_API_KEY}" \
      -H "Content-Type: application/json" \
-     --data '{"purge_everything":true}'
+     --data '{"purge_everything":true}' && echo "CloudFlare cache has been purged"
+
