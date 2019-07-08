c6ddb016a4c1101d28b32935aa9a25a7cabab504
Chris Short <chris@chrisshort.net>
2019-07-07 20:15:39 -0400

    New CI Pattern (Firebase)
    Signed-off-by: Chris Short <chris@chrisshort.net>

--- bin/cf_purge.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-curl -X POST "https://api.cloudflare.com/client/v4/zones/${CF_ZONE_ID}/purge_cache" \
-     -H "X-Auth-Email: ${CF_EMAIL}" \
-     -H "X-Auth-Key: ${CF_API_KEY}" \
-     -H "Content-Type: application/json" \
-     --data '{"purge_everything":true}'
